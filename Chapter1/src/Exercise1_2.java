f02161c22ac004a590c8319829bd6615c95c61ce
Jordan Podlucky <jordan.podlucky@ealas.org>
2016-02-21 19:15:01 -0800

    Test Exercise 2

---
+++ Chapter1/src/Exercise1_2.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jordan on 1/5/2016.
+ */
+public class Exercise1_2 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
