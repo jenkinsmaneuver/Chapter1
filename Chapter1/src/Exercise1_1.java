e6f80130452fafaa9ea82d80f93a0d8e2fbea269
Jordan Podlucky <jordan.podlucky@ealas.org>
2016-02-21 18:46:12 -0800

    First commit! Yay!

---
+++ Chapter1/src/Exercise1_1.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jordan on 1/5/2016.
+ */
+public class Exercise1_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+    }
+}
