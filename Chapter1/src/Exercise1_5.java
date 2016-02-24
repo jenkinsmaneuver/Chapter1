784e83c5878626651bc64f6ad6e387f6dde5c599
Jordan Podlucky <jordan.podlucky@ealas.org>
2016-02-24 15:20:54 -0800

    all files

---
+++ Chapter1/src/Exercise1_5.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jordan on 1/6/2016.
+ */
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
+    }
+}
---
+++ Chapter1/src/Exercise1_7.java
@@ -0,0 +1,9 @@
+
+public class Exercise1_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(4 * ((1.0)-(1.0/3)+(1.0/5)-(1.0/7)+(1.0/9)-(1.0/11)+(1.0/13)));
+	}
+
+}
